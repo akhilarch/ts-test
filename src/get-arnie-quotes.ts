7719b290cbbbab91320ad593d41539764d8f60f8
Akhil Biswas <application@Akhils-MBP.modem>
2022-02-28 22:54:55 +1100

    cleaned code

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -3,18 +3,12 @@ import { httpGet } from './mock-http-interface';
 type TResult = { 'Arnie Quote'?: string, 'FAILURE'?: string };
 
 export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
-  let result: TResult;
 
   const quotes = urls.map((url) => {
-    return httpGet(url)
-      .then((response) => {
-        if (response.status === 200) {
-          result = { 'Arnie Quote': JSON.parse(response.body).message };
-        } else {
-          result = { 'FAILURE': JSON.parse(response.body).message };
-        }
-        return result;
-      });
+    return httpGet(url).then((response) => {
+      const result: TResult = (response.status === 200) ? { 'Arnie Quote': JSON.parse(response.body).message } : { 'FAILURE': JSON.parse(response.body).message }
+      return result;
+    });
   });
   return Promise.all(quotes);
 };
