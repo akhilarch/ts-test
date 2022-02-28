97f2c4e5c5d03dea7cfa01bf2430df8204dcdf14
Akhil Biswas <application@Akhils-MBP.modem>
2022-02-28 22:58:55 +1100

    corrected the line spacing

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -3,7 +3,6 @@ import { httpGet } from './mock-http-interface';
 type TResult = { 'Arnie Quote'?: string, 'FAILURE'?: string };
 
 export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
-
   const quotes = urls.map((url) => {
     return httpGet(url).then((response) => {
       const result: TResult = (response.status === 200) ? { 'Arnie Quote': JSON.parse(response.body).message } : { 'FAILURE': JSON.parse(response.body).message }
